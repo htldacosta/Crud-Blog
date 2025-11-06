54c91e438ef33df0a3a7abb38b2104790285ef52
htldacosta <hitaludcosta@gmail.com>
2025-11-06 15:40:41 -0400

    the name UpdatePostRequest has been updated

--- src/main/java/com/blog/crud/infrastructure/web/dtos/UpdatePostRequest.java
+++ src/main/java/com/blog/crud/infrastructure/web/dtos/UpdatePostRequest.java
@@ -2,7 +2,7 @@ package com.blog.crud.infrastructure.web.dtos;
 
 import jakarta.validation.constraints.Size;
 
-public record PostResponse(
+public record UpdatePostRequest(
         @Size(min = 5, max = 100, message = "O título deve ter entre 5 e 100 caracteres.")
         String title,
 
